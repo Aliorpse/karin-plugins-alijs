58e53908f037538f8707519aac30f4f81e5a0e08
ud0d00 <Aliorpse@qq.com>
2025-03-15 15:25:59 +0800

    Create GHGraphs.js

---
+++ js/GHGraphs.js
@@ -0,0 +1,14 @@
+import karin, { segment } from 'node-karin'
+
+const regGH = /github\.com\/[a-zA-Z0-9-]{1,39}\/[a-zA-Z0-9_-]{1,100}/
+
+export const GHGraphs = karin.command(regGH, async (e) => {
+    const API_URL = "https://opengraph.githubassets.com"
+    const PLUGIN_ID = "karin-plugins-alijs_GHGraphs"
+
+    const match = e.msg.match(regGH)
+    if (!match) return
+
+    const repoPath = match[0].replace("github.com/", "")
+    return e.reply(segment.image(`${API_URL}/${PLUGIN_ID}/${repoPath}`))
+});
